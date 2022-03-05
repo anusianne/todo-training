78413a3e41f8063c72788a6a0e33648c4b08b094
anusianne <ann.ka.zielinska@gmail.com>
2022-03-05 22:26:48 +0100

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageModule } from './pages/home.page-module';
+import { AboutUsPageModule } from './pages/about-us.page-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ 
+        path: 'onas', 
+        loadChildren: () => AboutUsPageModule
+      },
+  { 
+        path: 'home', 
+        loadChildren: () => HomePageModule
+      }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), HomePageModule],
