08aa08129145954f7daca6923e455e7afb9c56a0
anusianne <ann.ka.zielinska@gmail.com>
2022-03-23 19:23:19 +0100

    firebaseinstall

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponentModule } from '@navigation';
-
+import { AngularFireModule} from '@angular/fire/compat';
+const firebaseConfig = {
+  apiKey: "AIzaSyBdPtI69OJB3kyF9g6TCVjVK3Rs4ykL9ng",
+  authDomain: "lowgular-course-3e18e.firebaseapp.com",
+  projectId: "lowgular-course-3e18e",
+  storageBucket: "lowgular-course-3e18e.appspot.com",
+  messagingSenderId: "771810143888",
+  appId: "1:771810143888:web:8799ab6a4cd7698b703215"
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(firebaseConfig),
     BrowserModule,
     AppRoutingModule,
     NavbarComponentModule
